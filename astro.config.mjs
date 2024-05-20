26c300e24d6ba5c28db935901e89b6be59e4c44d
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-05-20 11:50:18 -0700

    make icons local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
