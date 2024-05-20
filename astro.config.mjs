c5380304304d9819c3748860ced1a55417c504f3
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-05-20 10:52:51 -0700

    add icon types, fix breaking changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   root: "./src",
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
 });
