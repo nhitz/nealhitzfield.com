33483d456c431a93f35580e77f3392d2d279daad
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-05-31 09:24:54 -0700

    npx astro add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   root: "./src",
-  integrations: [tailwind(), svelte(), icon()],
-  server: { port: 8080 },
+  integrations: [tailwind(), svelte(), icon(), vue()],
+  server: {
+    port: 8080
+  }
 });
