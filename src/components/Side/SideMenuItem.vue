482a334e8b9ba9321f2ba2a2de433c31bfa6f820
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-06-02 14:16:20 -0700

    convert SideMenuItem to vue

---
+++ src/components/Side/SideMenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <a
+      :href="href"
+      class="flex gap-4 text-zinc-400 hover:text-zinc-100 py-3.5 px-5 font-semibold transition-all duration-300"
+    >
+      <component :is="iconName" class="h-6 w-6" />
+      <slot />
+    </a>
+  </li>
+</template>
+
+<script>
+import HomeIcon from "../icons/HomeIcon.vue";
+import SearchIcon from "../icons/SearchIcon.vue";
+import MediaIcon from "../icons/MediaIcon.vue";
+
+export default {
+  name: "SideMenuItem",
+  components: {
+    "HomeIcon": HomeIcon,
+    "SearchIcon": SearchIcon,
+    "MediaIcon": MediaIcon,
+  },
+  props: {
+    href: {
+      type: String,
+      default: "#",
+    },
+    iconName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
