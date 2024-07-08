bc78f801b77bfdb3959ca0f32d733bc749547226
Neal <34891912+nhitz@users.noreply.github.com>
2024-07-08 12:06:52 -0700

    feat: search (#46)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
-
+import pagefind from "astro-pagefind";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   root: "./src",
-  integrations: [tailwind(), svelte(), icon(), vue()],
+  integrations: [tailwind(), svelte(), icon(), vue(), pagefind()],
   server: {
     port: 8080,
   },
